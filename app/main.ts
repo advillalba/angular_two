3d75ed6f71f0f87456dd30ad2098b65db166f6fc
Alvaro <alvaro@adelgado-pc@delgado.com>
2017-05-28 13:27:03 +0200

    base

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+ 
+bootstrap(AppComponent);
