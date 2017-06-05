a6a40b8ffb7f6852250d625e3a1ddc1a4ab42c0d
Alvaro <alvaro@adelgado-pc@delgado.com>
2017-06-05 11:04:51 +0200

    Clase 20

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,33 @@
 // Importar el núcleo de Angular
-import {Component} from 'angular2/core';
+import {Component} from "angular2/core";
+import {Pelicula} from "./model/pelicula";
+
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-    templateUrl:"app/view/peliculas.html"
+    templateUrl:"app/view/peliculas.html",
+    styleUrls:["../assets/css/styles.css"]
     
 })
  
 // Clase del componente donde iran los datos y funcionalidades
 export class AppComponent {
 	public titulo:string = "Películas con Angular2";
-	public pelicula:string;
-	public director:string;
-	public anio:number;
-
+	public pelicula:Pelicula;
+    public mostrarDatos:boolean;
 
     constructor() {
-        this.pelicula = "Batman vs Superman";
-        this.director = "Zack Snider";
-        this.anio = 2016;
+        this.mostrarDatos = false;
+        this.pelicula = new Pelicula(1, "Batman vs Superman", "Zack Snider", 2016);
         
-        //this.holaMundo();
-    }
+        this.debug();
+         }
 
-    holaMundo() {
-        alert("Película: "+ this.pelicula + " - " + this.director + " - " + this.anio);
+    debug() {
+        console.log(this.pelicula);
+    }
+    onShowHide(value){
+        this.mostrarDatos = value;
     }
 }
